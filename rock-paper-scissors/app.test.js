0f744aa5b06cfa4a1b8dcafdae88e20b850fe7ee
Paulina G <paulinag@Paulinas-MacBook-Pro.local>
2024-08-06 09:43:17 +0100

    setup for ticket 5

---
+++ rock-paper-scissors/app.test.js
@@ -0,0 +1,5 @@
+Within app.test.js, plan and write test cases/scenarios for the following functions:
+calculateRoundResult - you should have a test for each permutation of valid inputs and assert that it returns the correct value
+calculateRoundResult - you should have a few tests for a few invalid inputs too and assert that it throws an error with the correct message
+calculateNewScores - you should test different score objects with different outcomes and assert that the correct value is returned
+generateComputerMove - you should have tests that verify and assert that the function can return either rock, paper or scissors.
